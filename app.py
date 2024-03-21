63b006b8b63adf24adb11d112427664c7ce298fe
Dhushyanth Ravichandran <dhushyanthravichandran@gmail.com>
2024-03-21 22:22:19 +0530

    added the form and gettingb input from there and viewing the result

--- app.py
+++ app.py
@@ -18,5 +18,14 @@ def predict_api():
     output=model.predict(np.array(list(data.values())).reshape(1,-1))
     result = int(output[0])  # Convert int64 to int
     return jsonify({'result': result})
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    print(np.array(data).reshape(1, -1))
+    output = model.predict(np.array(data).reshape(1, -1))
+    return render_template("home.html", predicted_text="The house price prediction is {}".format(output[0]))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
